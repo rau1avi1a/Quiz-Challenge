1903b04075d1158cd72e5b9f03df3766b0d53886
rau1avi1a <avila0118@icloud.com>
2023-04-09 22:39:18 -0700

    added functionality to buttons on javaScript file

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,60 +9,44 @@ var timerCount = 120;
 var startButton = document.querySelector(".startButton");
 var viewScores = document.querySelector(".viewScores");
 var backButton = document.querySelector(".backButton");
+var choiceButton = document.querySelector(".choiceButton");
 
 
 //screen selectors
 var startScreen = document.querySelector(".startScreen");
-var questionScreen = document.querySelector(".questionScreen");
+var questionScreen1 = document.querySelector(".questionScreen1");
+var questionScreen2 = document.querySelector(".questionScreen2");
+
 var scoreScreen = document.querySelector(".scoreScreen");
 var scoreBoardScreen = document.querySelector(".scoreBoardScreen");
+var viewHighscores = true;
 
 //score
-var scoreInitials = document.querySelector("#scoreInitials");
 var scoreSubmit = document.querySelector("#scoreSubmit");
-var storedScores = document.querySelector("#highscores");
-var scoreAmount
-var correct;
-var initials;
-var score;
+var highscores = document.querySelector("#highscores");
+var nameInput = document.querySelector("#name");
+let names = [];
+var score = 0;
 
 //quiz
 var question = document.querySelector(".question");
+var answerCorrect = document.querySelector("#answerCorrect");
+var answerWrong = document.querySelector("#answerWrong");
 
-var questionSet = [
-    "Who are you?",
-    "How are you?",
-    "Why are you?"
-];
-var answerSet1 = [
-    "1",
-    "2",
-    "3",
-    "4",
-]
-var answerSet2 = [
-    "5",
-    "6",
-    "7",
-    "8"
-]
-var answerSet3 = [
-    "9",
-    "10",
-    "11",
-    "12"
-]
 
 //Functions
 
-//This function will start the game
+//This function will start the quiz
 function startQuiz() {
+    //Disables View Highscores button
+    viewHighscores = false;
     //Hides start screen and shows questions
     startScreen.setAttribute("class", "hide");
-    questionScreen.setAttribute("class","showIndented");
     //Starts timer
-    startTimer();
+    //starts questions
+    questionScreen1.setAttribute("class","showIndented");
     quiz();
+    startTimer();
 }
 
 //Starts countdown
@@ -75,35 +59,77 @@ function startTimer() {
       if (timerCount == 0) {
         // Clears interval
         clearInterval(timer);
-        timerCount = 120;
-        questionScreen.setAttribute("class","hide");
         endQuiz();
+        timerCount = 120;
       }
     }, 1000);
 }
 
 //quiz
-function quiz () {
-    for (var i = 0; i < questionSet.length; i++) {
-        document.getElementById("question").innerHTML = questionSet[i];
-        document.getElementById("answer1").innerHTML = answerSet1[i-2];
-        document.getElementById("answer2").innerHTML = answerSet1[i-1];
-        document.getElementById("answer3").innerHTML = answerSet1[i];
-        document.getElementById("answer4").innerHTML = answerSet1[i+1];
+function quiz (event) {
+    if (choiceButton.contains(answerCorrect)) {
+        score++;
+    } 
+    if (choiceButton.contains(answerWrong)) {
+        timerCount-10;
+        timerElement.textContent = timerCount;
     }
+
 
 }
 
 //This function ends the quiz
 function endQuiz() {
+    questionScreen1.setAttribute("class","hide");
+    questionScreen2.setAttribute("class","hide");
     scoreScreen.setAttribute("class","showCenter");
 }
 
 //This function submits the quiz score
 function submitQuiz(event) {
     event.preventDefault();
+
+    var userInput = nameInput.value.trim();
+    if (userInput === "") {
+        return;
+    }
+    names.push(userInput);
+    localStorage.setItem("names", JSON.stringify(names));
     scoreScreen.setAttribute("class","hide");
+    addScore();
+    nameInput.value="";
     scoreBoardScreen.setAttribute("class","showCenter");
+}
+
+//This function adds a score to the scoreboard
+function addScore() {
+    var li = document.createElement("li");
+    li.textContent = nameInput.value.trim() + " - " + score;
+    highscores.appendChild(li);
+    //resets score
+    score = 0;
+}
+
+//This function adds stored scores to the scoreboard
+function renderScores() {
+    nameInput.value = "";
+
+    for (var i = 0; i < names.length; i++) {
+        var nameList = names[i];
+        var li = document.createElement("li");
+        li.textContent = nameList + " - " + score;
+        li.setAttribute("data-index", i);
+        highscores.appendChild(li);
+      }
+}
+
+//This function retrieves the scores from local storage
+function init() {
+    var scoreBoard = JSON.parse(localStorage.getItem("names"));
+    if (scoreBoard !== null) {
+      names = scoreBoard;
+    }
+    renderScores();
 }
 
 //Takes user back to main screen
@@ -111,36 +137,30 @@ function goBack() {
     timerElement.textContent = timerCount;
     scoreBoardScreen.setAttribute("class", "hide");
     startScreen.setAttribute("class", "showCenter");
-}
-
-//This function adds scores to the scoreboard
-function renderScores() {
-    highscores.innerHTML = "";
-    for (var i = 0; i < todos.length; i++) {
-      var todo = todos[i];
-  
-      var li = document.createElement("li");
-      li.textContent = todo;
-      li.setAttribute("data-index", i);
-  
-      var button = document.createElement("button");
-      button.textContent = "Complete ✔️";
-  
-      li.appendChild(button);
-      todoList.appendChild(li);
-    }
-  
+    viewHighscores = true;
 }
 
 //This function lets user view scoreboard
 function viewScoreBoard (event) {
-    scoreBoardScreen.setAttribute("class", "showCenter");
-
+    event.preventDefault();
+    if (viewHighscores == true) {
+        startScreen.setAttribute("class", "hide");
+        scoreBoardScreen.setAttribute("class", "showCenter");
+    }
 }
+
 
 //Event Listeners
 
+//Starts quiz
 startButton.addEventListener("click", startQuiz);
+//Views the scoreboard
 viewScores.addEventListener("click", viewScoreBoard);
+//Goes back to start screen
 backButton.addEventListener("click", goBack);
+//Submits initials to scoreboard
 scoreSubmit.addEventListener("submit", submitQuiz);
+//Selects answer
+choiceButton.addEventListener("click", quiz);
+
+init();
